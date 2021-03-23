40b5cc5686369e6c985384c103e49c26971881c2
Yuuki Bamba <yuukiBamba@gmail.com>
2021-03-24 00:41:40 +0900

    【Environment】C#ManagedPlugins追加

---
+++ ManagedPlugins/ManagedPlugins/sources/Define.cs
@@ -0,0 +1,18 @@
+﻿namespace Yuki.ManagedPlugins
+{
+    /// <summary>
+    /// 定数宣言
+    /// </summary>
+    public class Define
+    {
+        /// <summary>
+        /// コンストラクタ隠蔽
+        /// </summary>
+        private Define() { }
+        public const int NOTHING_LAYER_VALUE = 0;
+        public const int EVERYTHING_LAYER_VALUE = ~0;
+        public const float MAX_ALPHA = 1.0f;
+        public const float MIN_ALPHA = 0.0f;
+        public const string ANDROID_LOCAL_HEADER = "file:///";
+    }
+}
