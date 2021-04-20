08e0578289abd2172729bec28eb790c27b2108a5
Yuuki Bamba <yuukiBamba@gmail.com>
2021-04-21 00:19:28 +0900

    【develop】プロトコル周りの定義方法を変更

---
+++ Server/Server/sources/Protocol/Impl/ClientStreamingImpl.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Grpc.Core;
+using System.Threading;
+using System.Threading.Tasks;
+using Google.Protobuf.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Server.gRPC;
+
+namespace Server.Impl
+{
+    class ClientStreamingImpl : ClientStreaming.ClientStreamingBase
+    {
+
+    }
+}
--- Server/Server/sources/Thread/NetworkThread.cs
+++ Server/Server/sources/Thread/NetworkThread.cs
@@ -1,66 +1,66 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using Grpc.Core;
 using System.Threading;
 using System.Threading.Tasks;
 using Google.Protobuf.Collections;
 using Framework;
 using Server.gRPC;
+using Server.Impl;
 
 class NetworkThread : AbstractThread
-{
-    public NetworkThread(uint fps) : base(fps) { instance = this; }
-    ~NetworkThread() { }
-
-    private const string IPAddress = "127.0.0.1";
-    private const int Port = 1122;
-
-    private static NetworkThread instance { get; set; }
-
-    #region protocol
-    class PingImpl : Ping.PingBase
     {
-        public override Task<S2C_Ping_Response> Reply(C2S_Ping_Request request, ServerCallContext context)
-        {
-            var ping = instance != null ? instance.DeltaTimeWatch.Elapsed.TotalMilliseconds : -1;
-            Console.WriteLine($"ping:{ping}");
-            return Task.FromResult(new S2C_Ping_Response
-            {
-                Ping = ping
-            });
-        }
-    }
-    #endregion
+    public NetworkThread(uint fps) : base(fps) { }
+        ~NetworkThread() { }
 
-    public override void StartThread()
-    {
-        thread = new Thread(new ThreadStart(() => { base.StartThread(); }));
-        Setup();
-        try
+        private const string IPAddress = "127.0.0.1";
+        private const int Port = 1122;
+
+        public override void StartThread()
         {
-            Grpc.Core.Server server = new Grpc.Core.Server
+            thread = new Thread(new ThreadStart(() => { base.StartThread(); }));
+            Setup();
+            try
             {
-                Services = { Ping.BindService(new PingImpl()) },
-                Ports = { new ServerPort(IPAddress, Port, ServerCredentials.Insecure) }
+            Grpc.Core.Server server = new Grpc.Core.Server(
+                new List<ChannelOption>
+                {
+                    new ChannelOption(ChannelOptions.MaxReceiveMessageLength,8388608),
+                })
+            {
+                Services =
+                {
+                    Unary.BindService(new UnaryImpl()),
+                    ServerStreaming.BindService(new ServerStreamingImpl()),
+                    ClientStreaming.BindService(new ClientStreamingImpl()),
+                    BidirectionalStreaming.BindService(new BidirectionalStreamingImpl()),
+                },
+                Ports =
+                {
+                    new ServerPort(IPAddress, Port, ServerCredentials.Insecure)
+                }
+
             };
             server.Start();
         }
         catch (Exception e)
-        {
-            Console.WriteLine($"エラー\n{e.Message}");
+            {
+                Console.WriteLine($"エラー\n{e.Message}");
+            }
+            thread.Start();
         }
-        thread.Start();
-    }
 
-    public override void Process()
-    {
-        //Console.WriteLine($"{instance.DeltaTimeWatch.Elapsed.TotalMilliseconds}");
-    }
+        public override void Process()
+        {
+            //Console.WriteLine($"{instance.DeltaTimeWatch.Elapsed.TotalMilliseconds}");
+        }
 
-    /// <summary>
-    /// 初期化
-    /// </summary>
-    private void Setup()
-    {
+        /// <summary>
+        /// 初期化
+        /// </summary>
+        private void Setup()
+        {
 
+        }
     }
-}
