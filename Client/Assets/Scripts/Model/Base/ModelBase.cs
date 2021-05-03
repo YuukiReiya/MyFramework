4fd099050dc9d5eb0a5c47c74eee0dab909fd072
Yuuki Bamba <yuukiBamba@gmail.com>
2021-05-03 10:26:56 +0900

    【fix】MVCモデルクラス準備
    チャット用の管理モデル実装

---
+++ Client/Assets/Scripts/Model/Base/ModelBase.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Model.Base
+{
+    public class ModelBase<T> where T : class, new()
+    {
+        private static T instance = null;
+        public static T Instance 
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new T();
+                }
+                return instance;
+            }
+        }
+    }
+}
