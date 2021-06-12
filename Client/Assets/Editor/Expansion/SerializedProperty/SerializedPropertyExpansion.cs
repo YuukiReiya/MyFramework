c8b8c73bde86f76a3d0c2a968457a11df1b97ece
Yuuki Bamba <yuukiBamba@gmail.com>
2021-06-12 22:57:33 +0900

    develop Editor拡張用にSerializedPropertyに拡張メソッド用意。

---
+++ Client/Assets/Editor/Expansion/SerializedProperty/SerializedPropertyExpansion.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+
+public static class SerializedPropertyExpansion
+{
+    /// <summary>
+    /// MonoBehaviour継承クラスか.
+    /// </summary>
+    /// <param name="property"></param>
+    /// <returns></returns>
+    public static bool IsMonoBehaviour(this SerializedProperty property)
+    {
+        return property.serializedObject.targetObject.GetType().IsSubclassOf(typeof(UnityEngine.MonoBehaviour));
+    }
+
+}
