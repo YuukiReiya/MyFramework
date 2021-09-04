1df36749ddb51968800515a113e7ca29e591cb8f
Yuuki Bamba <yuukiBamba@gmail.com>
2021-09-04 11:02:21 +0900

    【develop】upload処理追加
    iniで指定したフォルダをzip圧縮してDriveのRootにアップロードする。

--- GoogleDrive/GoogleDriveManager/Uploader/EntryPoint.cs
+++ GoogleDrive/GoogleDriveManager/Uploader/EntryPoint.cs
@@ -10,6 +10,9 @@ namespace Uploader
     {
         const string _CredentialsFilePath = @"./res/credentials.json";
         const string _IniFilePath = @"./res/system.ini";
+        const string _TempArchiveDirectory = @"./temp/";
+        const string _UnityClientSection = "COMMON";
+        const string _UnityClientKey = "UNITY_PROJECT_PATH";
 #if false
         // Upload方法は3種類あるけど、ぶっちゃけResumableだけでいい。
         const string _SectionHeader = "UPLOAD";
@@ -28,7 +31,6 @@ namespace Uploader
                 return;
             }
 
-
             var uploadFiles = targetNames;
 
             if (!uploadFiles.Any())
@@ -40,21 +42,53 @@ namespace Uploader
                     false
 #endif
                     );
-                // 
-                //ini.DataList.Where()
+                var clientAssetsPath = ini.DataList.Find(data => data.Item1 == _UnityClientSection && data.Item2 == _UnityClientKey).Item3;
                 var dataCollection = ini.DataList.Where(sec => sec.Item1 == _Section);
 
-                if (model.IsExist(""))
+                foreach(var data in dataCollection)
                 {
+                    var tempDir = _TempArchiveDirectory + Path.GetFileName(data.Item2);
 
+                    // 生成先の親ディレクトリが無ければ作る
+                    DirectoryInfo directory = new DirectoryInfo(tempDir);
+                    if (directory.Parent != null && !directory.Parent.Exists)
+                    {
+                        Directory.CreateDirectory(directory.Parent.FullName);
+                    }
+
+                    ZipModel.Compression(data.Item2, tempDir);
+                }
+
+                // GDrive上にArchiveが挙がっているのか確認用に表示する.
+                Console.WriteLine($"====================================================================");
+                Console.WriteLine($"GoogleDrive Uploaded Files.");
+                Console.WriteLine($"====================================================================");
+                model.ShowUploadedList();
+                Console.WriteLine($"====================================================================");
+
+                var archivedFiles = Directory.GetFiles(_TempArchiveDirectory).Select(path => Path.GetFileName(path));
+                foreach (var arcFileName in archivedFiles)
+                {
+                    Console.WriteLine($"arcedName:{arcFileName}");
+                    if (model.IsExist(arcFileName))
+                    {
+                        Console.WriteLine($"\"{arcFileName}\" is already exist. skip this file.");
+                        continue;
+                    }
+                    model.Upload($"{_TempArchiveDirectory}{arcFileName}", "");
                 }
                 Console.Read();
 
-                return;
-                model.Upload(new[] { "test" });
+                // zip化したアーカイブファイル先
+                var arcFileNames = Directory.GetFiles(_TempArchiveDirectory);
 
-                Console.WriteLine("Complete upload!.");
-                Console.Read();
+#if DEBUG
+                foreach(var name in arcFileNames)
+                {
+                    Console.Write($"zip:{name} => {Path.GetFullPath(name)}");
+                }
+#endif
+                return;
             }
         }
     }
