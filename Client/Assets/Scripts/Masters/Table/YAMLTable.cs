f692e232e221a5898ca0d95240969776b3fa5c10
Yuuki Bamba <yuukiBamba@gmail.com>
2021-12-02 20:05:00 +0900

    develop YAMLテーブルクラス
    既定テーブルクラスの使いまわしが実装上出来なくなった(されなくなった)ので
    実装上のお飾り。
    ひとまず実装だけしておく← 使うとは言ってない

---
+++ Client/Assets/Scripts/Masters/Table/YAMLTable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Masters
+{
+    public class YAMLTable : TableBase
+    {
+        public const string Extension = ".yaml";
+        public const string Extension2 = ".yml";
+
+        public YAMLTable(string path) : base(path) { }
+    }
+}
