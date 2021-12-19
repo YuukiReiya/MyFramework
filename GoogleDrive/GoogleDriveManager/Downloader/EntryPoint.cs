0ea4c8409d2481f2b6bb6263fbfb83b5ba09a8e6
Yuuki Bamba <yuukiBamba@gmail.com>
2021-12-19 23:32:25 +0900

    bugfix Downloader側の調整

--- GoogleDrive/GoogleDriveManager/Downloader/EntryPoint.cs
+++ GoogleDrive/GoogleDriveManager/Downloader/EntryPoint.cs
@@ -9,16 +9,56 @@ namespace Downloader
     class EntryPoint
     {
         const string _CredentialsFilePath = @"./res/credentials.json";
+        const string _TokenFolderPath = @"./res/token.json";
         const string _IniFilePath = @"./res/system.ini";
         const string _TempArchiveDirectory = @"./temp/";
         const string _DownloadSection = "DOWNLOAD";
         const string _UncompressionSection = "UNCOMPRESSION";
 
-        static void Main(string[] targetFileNames)
+        static void Main(string[] resourcesDirectory)
         {
             var model = GoogleServiceModel.Instance;
 
-            var result = model.Setup(Path.GetFullPath(_CredentialsFilePath),"");
+            if (resourcesDirectory.Length < 1)
+            {
+                //早期リターン.
+                //Console.WriteLine($"invalid application argments. [0]:resource root directory.");
+                //return;
+
+                //NOTE: bin\$(Configuration)\net5.0\*.exe
+                resourcesDirectory = new[] { $"../../../" };
+            }
+
+            var resDir = resourcesDirectory[0];
+            var credentialPath = resDir + _CredentialsFilePath;
+
+            if (!File.Exists(credentialPath))
+            {
+                Console.WriteLine($"credentials file is not exist. > {credentialPath}{Environment.NewLine}[FullPath]:{Path.GetFullPath(credentialPath)}");
+                Console.ReadKey();
+                return;
+            }
+
+            var tokenFolderPath = resDir + _TokenFolderPath;
+
+#if DEBUG//RELEASEビルドなら無い場合でもバイナリ直下に作る.
+            if (!Directory.Exists(tokenFolderPath))
+            {
+                Console.WriteLine($"token.json folder is not exist. > {tokenFolderPath}{Environment.NewLine}[FullPath]:{Path.GetFullPath(tokenFolderPath)}");
+                Console.ReadKey();
+                return;
+            }
+#endif
+
+            var iniPath = resDir + _IniFilePath;
+            if (!File.Exists(iniPath))
+            {
+                Console.WriteLine($"ini file is not exist. > {iniPath}");
+                Console.ReadKey();
+                return;
+            }
+
+            var result = model.Setup(Path.GetFullPath(credentialPath), Path.GetFullPath(tokenFolderPath));
             if (result != GoogleServiceModel.Result.Success)
             {
                 Console.WriteLine("Setup Failed.");
@@ -37,48 +77,44 @@ namespace Downloader
             model.ShowUploadedFiles();
 #endif
 
-            var downloadFiles = targetFileNames;
             // 引数無し実行なら.iniから読み込む
-            if (!downloadFiles.Any())
-            {
-                var ini = new IniReader(Path.GetFullPath(_IniFilePath),
-                    #if DEBUG
+            var ini = new IniReader(Path.GetFullPath(iniPath),
+#if DEBUG
                     true
-                    #else
+#else
                     false
-                    #endif
+#endif
                     );
-                var e = ini.DataList.Where(sec => sec.Item1 == _DownloadSection).GetEnumerator();
+            var e = ini.DataList.Where(sec => sec.Item1 == _DownloadSection).GetEnumerator();
 
-                // ダウンロード(一時保存)
-                while (e.MoveNext())
+            // ダウンロード(一時保存)
+            while (e.MoveNext())
+            {
+                var current = e.Current;
+                var drivePath = current.Item3;
+
+                // ダウンロード先のファイルがない.
+                if (!model.IsExist(drivePath))
                 {
-                    var current = e.Current;
-                    var drivePath = current.Item3;
+                    Console.WriteLine($"Download failed.{Environment.NewLine}Not found file! > {drivePath}");
+                    continue;
+                }
 
-                    // ダウンロード先のファイルがない.
-                    if (!model.IsExist(drivePath))
-                    {
-                        Console.WriteLine($"Download failed.{Environment.NewLine}Not found file! > {drivePath}");
-                        continue;
-                    }
+                var downloadPath = _TempArchiveDirectory + drivePath;
 
-                    var downloadPath = _TempArchiveDirectory + drivePath;
+                // 保存先のディレクトリが無ければ作る.
+                var info = new DirectoryInfo(downloadPath);
+                if (info.Parent != null && !info.Parent.Exists)
+                {
+                    Directory.CreateDirectory(info.Parent.FullName);
+                }
+                model.Download(drivePath, downloadPath);
 
-                    // 保存先のディレクトリが無ければ作る.
-                    var info = new DirectoryInfo(downloadPath);
-                    if (info.Parent != null && !info.Parent.Exists)
-                    {
-                        Directory.CreateDirectory(info.Parent.FullName);
-                    }
-                    model.Download(drivePath, downloadPath);
-
-                    // 解凍.
-                    var target = ini.DataList.FirstOrDefault(data => data.Item1 == _UncompressionSection && data.Item2 == current.Item2);
-                    if (target != default)
-                    {
-                        ZipModel.Uncompression(downloadPath, target.Item3);
-                    }
+                // 解凍.
+                var target = ini.DataList.FirstOrDefault(data => data.Item1 == _UncompressionSection && data.Item2 == current.Item2);
+                if (target != default)
+                {
+                    ZipModel.Uncompression(downloadPath, target.Item3);
                 }
             }
             Console.Read();
