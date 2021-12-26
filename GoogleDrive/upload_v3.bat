e18d39a2e2afe0aa4b649ebb15d9998f4956aede
Yuuki Bamba <yuukiBamba@gmail.com>
2021-12-26 18:22:02 +0900

    fix config.bat.xml削除

--- GoogleDrive/upload_v3.bat
+++ GoogleDrive/upload_v3.bat
@@ -1,10 +1,11 @@
 @echo off
 set DEBUG_EXE_PATH=%~dp0bin\Debug\net5.0\Uploader.exe
-set RESOURCE_ROOT=%~dp0res\config.bat.xml
+set RESOURCE_ROOT=%~dp0res\config.xml
 
 REM DEBUGバイナリ優先
 if exist %DEBUG_EXE_PATH% (
 
+    cd %~dp0bin\Debug\net5.0\
     call %DEBUG_EXE_PATH% %RESOURCE_ROOT%
     exit /b 0
 )
@@ -14,6 +15,7 @@ set RELEASE_EXE=%~dp0bin\Release\net5.0\Uploader.exe
 REM RELEASEビルド
 if exist %RELEASE_EXE% (
 
+    cd %~dp0bin\Release\net5.0\
     call %RELEASE_EXE% %RESOURCE_ROOT%
     exit /b 0
 )
