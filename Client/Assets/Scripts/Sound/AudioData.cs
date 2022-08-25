b3813c0ba4631346dd744a949bdba4473ad0fde9
Yuuki Bamba <yuukiBamba@gmail.com>
2022-08-25 14:59:10 +0900

    bugfix 必要スクリプトが漏れてたので追加
    加えて上がっていないメタファイルをあげた

---
+++ Client/Assets/Scripts/Sound/AudioData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sound
+{
+    public class AudioData
+    {
+        public AudioClip Clip { get; private set; }
+    }
+}
